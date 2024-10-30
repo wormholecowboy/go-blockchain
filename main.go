125c82018e8dac0c2321276315079b18ad2321bc
wormhole cowboy <wormholecowboy@gmail.com>
2024-10-29 23:38:49 -0400

    Extracted blockchain to package

--- main.go
+++ main.go
@@ -1,54 +1,17 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
 	"fmt"
+  "github.com/wormholecowboy/go-blockchain/blockchain"
 )
 
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-type Blockchain struct {
-	blocks []*Block
-}
-
-func (b *Blockchain) AddBlock(data string) {
-	prevBlock := b.blocks[len(b.blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	b.blocks = append(b.blocks, newBlock)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{Genesis()}}
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-}
-
 func main() {
-	chain := InitBlockchain()
+	chain := blockchain.InitBlockchain()
 
 	chain.AddBlock("second")
 	chain.AddBlock("third")
 
-	for _, block := range chain.blocks {
+	for _, block := range chain.Blocks {
 		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
